2ae27193b4b00b17e62b65b50344f726dcfa3cbd
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-06-06 19:11:21 +0300

    5-6 using typing

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,9 +2,10 @@
 """
 Sum of all elements in a list
 """
+import typing
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: typing.List[float]) -> float:
     """
     This function returns the sum of all elements in the input list.
     """
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,8 +2,9 @@
 """
 sum_mixed_list module - sum_mixed_list function.
 """
+import typing
 
 
-def sum_mixed_list(mxd_lst: list[int | float]) -> float:
+def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]) -> float:
     """Sum the elements of a list of integers and floats."""
     return sum(mxd_lst)
