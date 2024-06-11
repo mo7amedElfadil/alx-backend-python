35694bd340a19fc2d0045b9f88745c3d81ab1da3
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-06-11 16:50:36 +0300

    trying older version of generator typing

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,10 +9,10 @@ a random number between 0 and 10 n times.
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     This coroutine will yield a random number between 0 and 10 n times.
     """
